d73547fa093cc596c51790eebba98abb6f11fe84
Kyla Do <kd.katiedo3@gmail.com>
2024-04-29 21:57:21 -0700

    Create client.py

---
+++ client.py
@@ -0,0 +1,35 @@
+import socket
+import ipaddress
+import threading
+import time
+import contextlib
+import errno
+
+maxPacketSize = 1024
+defaultPort = 3000 # TODO: Change this to your expected port
+serverIP = '127.0.0.1' #TODO: Change this to your instance IP
+
+tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+try:
+    tcpPort = int(input("Please enter the TCP port of the host..."));
+except:
+    tcpPort = 0;
+if tcpPort == 0:
+    tcpPort = defaultPort;
+tcpSocket.connect((serverIP, tcpPort));
+
+clientMessage = "";
+while clientMessage != "exit":
+    clientMessage = input("Please type the message that you'd like to send (Or type \"exit\" to exit):\n>");
+
+    #TODO: Send the message to your server
+    tcpSocket.sendall(clientMessage.encode())
+
+    #TODO: Receive a reply from the server for the best highway to take
+    server_response = tcpSocket.recv(1024).decode()
+    print("Server Response:", server_response)
+    
+    #TODO: Print the best highway to take
+    
+tcpSocket.close();
+
