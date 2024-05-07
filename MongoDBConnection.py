d65bf3ee95ca8aec5655a93f7b9d5bff44cfaa2a
Kyla Do <kd.katiedo3@gmail.com>
2024-05-06 22:22:20 -0700

    Update MongoDBConnection.py

--- MongoDBConnection.py
+++ MongoDBConnection.py
@@ -4,6 +4,7 @@ import threading
 import pymongo
 from datetime import datetime, timedelta
 import time
+import certifi
 
 DBName = "test" #Use this to change which Database we're accessing
 connectionURL = "mongodb+srv://kylado:Ilovececs327@cluster0.06rwogu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Put your database URL here
@@ -14,40 +15,39 @@ def QueryToList(query):
   #HINT: MongoDB queries are iterable
 
 def QueryDatabase() -> []:
-	global DBName
-	global connectionURL
-	global currentDBName
-	global running
-	global filterTime
-	global sensorTable
-	cluster = None
-	client = None
-	db = None
-	try:
-		cluster = connectionURL
-		client = MongoClient(cluster)
-		db = client[DBName]
-		print("Database collections: ", db.list_collection_names())
+    global DBName
+    global connectionURL
+    global currentDBName
+    global running
+    global filterTime
+    global sensorTable
+    cluster = None
+    client = None
+    db = None
+    try:
+        cluster = connectionURL
+        client = MongoClient(cluster, tlsCAFile=certifi.where())
+        db = client[DBName]
+        print("Database collections: ", db.list_collection_names())
 
-		#We first ask the user which collection they'd like to draw from.
-		sensorTable = db[sensorTable]
-		print("Table:", sensorTable)
-		#We convert the cursor that mongo gives us to a list for easier iteration.
-		timeCutOff = datetime.now() - timedelta(minutes=0) #TODO: Set how many minutes you allow
+        #We first ask the user which collection they'd like to draw from.
+        sensorTable = db[sensorTable]
+        print("Table:", sensorTable)
+        #We convert the cursor that mongo gives us to a list for easier iteration.
+        timeCutOff = datetime.now() - timedelta(minutes=5) #TODO: Set how many minutes you allow
 
-		oldDocuments = QueryToList(sensorTable.find({"time":{"$gte":timeCutOff}}))
-		currentDocuments = QueryToList(sensorTable.find({"time":{"$lte":timeCutOff}}))
+        oldDocuments = QueryToList(sensorTable.find({"time":{"$gte":timeCutOff}}))
+        currentDocuments = QueryToList(sensorTable.find({"time":{"$lte":timeCutOff}}))
 
-		print("Current Docs:",currentDocuments)
-		print("Old Docs:",oldDocuments)
+        print("Current Docs:",currentDocuments)
+        print("Old Docs:",oldDocuments)
 
-		#TODO: Parse the documents that you get back for the sensor data that you need
-		#Return that sensor data as a list
-		return currentDocuments
+        #TODO: Parse the documents that you get back for the sensor data that you need
+        #Return that sensor data as a list
+        return currentDocuments
 
 
-	except Exception as e:
-		print("Please make sure that this machine's IP has access to MongoDB.")
-		print("Error:",e)
-		exit(0)
-
+    except Exception as e:
+        print("Please make sure that this machine's IP has access to MongoDB.")
+        print("Error:",e)
+        exit(0)
